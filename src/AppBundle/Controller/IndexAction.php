2643628abd7efa6c33a6605a8f160891ff0aaadd
Bryan <bryan.hong.1988@gmail.com>
2016-04-26 19:46:53 +1000

    Bryan Add: add template

--- src/AppBundle/Controller/IndexAction.php
+++ src/AppBundle/Controller/IndexAction.php
@@ -1,12 +1,19 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: bryan
+ * Date: 26/04/2016
+ * Time: 7:02 PM
+ */
 
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class IndexAction extends Controller
 {
     /**
      * @Route("/", name="homepage")
@@ -14,8 +21,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('index.html.twig');
     }
+
 }
