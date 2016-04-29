3e5644fc5f40a1e4fddb72b80f92b8fdebb8815e
Jmily <weiyi.li713@gmail.com>
2016-04-29 17:39:04 +1000

    test data

---
+++ web/js/city.js
@@ -0,0 +1,13 @@
+/**
+ * Created by emilychen on 29/04/2016.
+ */
+var melbourne = {"job_variety": "1200", "job_popularity": "2200" , "job_volume":"22039"};
+var sydney = {"job_variety": "1100", "job_popularity": "2400" , "job_volume":"24098"};
+var brisbane = {"job_variety": "700", "job_popularity": "1200" , "job_volume":"12908"};
+var perth = {"job_variety": "680", "job_popularity": "1150" , "job_volume":"18902"};
+var adelaide = {"job_variety": "520", "job_popularity": "1290" , "job_volume":"12876"};
+var goldcoast = {"job_variety": "400", "job_popularity": "1390" , "job_volume":"19087"};
+var canberra = {"job_variety": "758", "job_popularity": "1890" , "job_volume":"16889"};
+var newcastle ={"job_variety": "678", "job_popularity": "900" , "job_volume":"18976"};
+
+
