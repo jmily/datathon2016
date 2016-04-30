5cdb65b3a54ae2d70e0568c1fb748bce8b38162c
Jmily <weiyi.li713@gmail.com>
2016-04-30 15:40:12 +1000

    job  data

--- web/js/city.js
+++ web/js/city.js
@@ -1,15 +1,14 @@
 
 var cities = {
-    "Melbourne": {"lat": "-37.8141", "lng": "144.9633", "job_variety": "1126", "job_popularity": "2200" , "job_volume":"1126"},
-    "Sydney": {"lat": "-33.8675", "lng": "151.207", "job_variety": "1054", "job_popularity": "2400" , "job_volume":"1054"},
-    "Brisbane": {"lat": "-27.471", "lng": "153.0234", "job_variety": "615", "job_popularity": "1200" , "job_volume":"615"},
-    "Perth": {"lat": "-31.953", "lng": "115.8575", "job_variety": "680", "job_popularity": "1150" , "job_volume":"953"},
-    "Adelaide": {"lat": "-34.9286", "lng": "138.6", "job_variety": "520", "job_popularity": "1290" , "job_volume":"520"},
-    // "Goldcoast": {"lat": "-28.0173", "lng": "153.4257", "job_variety": "400", "job_popularity": "1390" , "job_volume":"19087"},
-    "Canberra": {"lat": "-35.282", "lng": "149.1287", "job_variety": "758", "job_popularity": "1890" , "job_volume":"231"},
-    "Newcastle": {"lat": "-32.9185", "lng": "151.6071", "job_variety": "678", "job_popularity": "900" , "job_volume":"412"},
-    "Tasmania": {"lat": "-41.365", "lng": "146.6285", "job_variety": "554", "job_popularity": "789" , "job_volume":"15"},
-    "Darwin": {"lat": "-12.4628", "lng": "130.8418", "job_variety": "432", "job_popularity": "678" , "job_volume":"226"}
-}
+    "Melbourne": {"lat": "-37.8141", "lng": "144.9633", "job_variety": "216", "job_popularity": "6784" , "job_volume":"1126"},
+    "Sydney": {"lat": "-33.8675", "lng": "151.207", "job_variety": "221", "job_popularity": "5645" , "job_volume":"1054"},
+    "Brisbane": {"lat": "-27.471", "lng": "153.0234", "job_variety": "171", "job_popularity": "5271" , "job_volume":"615"},
+    "Perth": {"lat": "-31.953", "lng": "115.8575", "job_variety": "155", "job_popularity": "4863" , "job_volume":"953"},
+    "Adelaide": {"lat": "-34.9286", "lng": "138.6", "job_variety": "126", "job_popularity": "3211" , "job_volume":"520"},
+    "Canberra": {"lat": "-35.282", "lng": "149.1287", "job_variety": "58", "job_popularity": "594" , "job_volume":"231"},
+    "Newcastle": {"lat": "-32.9185", "lng": "151.6071", "job_variety": "68", "job_popularity": "1433" , "job_volume":"412"},
+    "Tasmania": {"lat": "-41.365", "lng": "146.6285", "job_variety": "31", "job_popularity": "118" , "job_volume":"15"},
+    "Darwin": {"lat": "-12.4628", "lng": "130.8418", "job_variety": "58", "job_popularity": "744" , "job_volume":"226"}
+};
 
 
