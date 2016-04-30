f66c5f0138fdd38b65037cd2eedb6d45bf6aa89a
Jmily <weiyi.li713@gmail.com>
2016-04-30 18:59:40 +1000

    classification data

--- web/js/city.js
+++ web/js/city.js
@@ -7,12 +7,22 @@ var cities = {
     "Adelaide": {"lat": "-34.9286", "lng": "138.6", "job_variety": "126", "job_popularity": "3211" , "job_volume":"520"},
     "Canberra": {"lat": "-35.282", "lng": "149.1287", "job_variety": "58", "job_popularity": "594" , "job_volume":"231"},
     "Newcastle": {"lat": "-32.9185", "lng": "151.6071", "job_variety": "68", "job_popularity": "1433" , "job_volume":"412"},
-    "Tasmania": {"lat": "-41.365", "lng": "146.6285", "job_variety": "31", "job_popularity": "118" , "job_volume":"15"},
+    "Tasmania": {"lat": "-41.365", "lng": "146.6285", "job_variety": "57", "job_popularity": "532" , "job_volume":"322"},
     "Darwin": {"lat": "-12.4628", "lng": "130.8418", "job_variety": "58", "job_popularity": "744" , "job_volume":"226"}
 };
 
 
 
 
-
+var topJobClassification = {
+    "Melbourne": {"Government - State": "109", "Administrative Assistants":"102","Assembly & Process Work":"62","Pickers & Packers": "54","Retail Assistants":"54"},
+    "Sydney": {"Government - State": "157", "Administrative Assistants": "69", "Retail Assistants": "54","Couriers, Drivers & Postal Services":"38","Customer Service - Call Centre":"38"},
+    "Brisbane": {"Government - State": "256","Bar & Beverage Staff":"74","Administrative Assistants":"49","Couriers, Drivers & Postal Services":"43","Retail Assistants":"31"},
+    "Perth": {"Couriers, Drivers & Postal Services": "51", "Bar & Beverage Staff": "48","Government - State":"45","Administrative Assistants":"40","Mining - Engineering & Maintenance":"27"},
+    "Adelaide": {"Government - State":"58","Assembly & Process Work":"32","Administrative Assistants":"26","Aged & Disability Support":"25","Bar & Beverage Staff":"18"},
+    "Canberra":{"Government - Federal":"15","Couriers, Drivers & Postal Services":"11","Management - Universities":"8","Management - Department/Assistant":"8","Government - State":"7"},
+    "Newcastle":{"Government - State":"39","Aged & Disability Support":"10","Customer Service - Call Centre":"8","Administrative Assistants":"8","Automotive Trades":"5"},
+    "Tasmania": {"Government - State":"37","Administrative Assistants":"11","Government - Local":"7","Couriers, Drivers & Postal Services":"5","Bar & Beverage Staff":"5"},
+    "Darwin": {"Government - Local":"18","Couriers, Drivers & Postal Services":"10","Bar & Beverage Staff":"9","Management - Department/Assistant":"8","Aged & Disability Support":"8"}
+};
 
